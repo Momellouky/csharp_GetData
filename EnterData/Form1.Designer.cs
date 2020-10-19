d5d874343bd50ed055af6b271cf8abcc2b326686
Mohamed Mellouky <contact.mellouky@gmail.com>
2020-10-19 10:12:53 +0100

    initial commit

---
+++ EnterData/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace EnterData
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Header = new System.Windows.Forms.Label();
+            this.BackGround = new System.Windows.Forms.Label();
+            this.TxtId = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.LblId = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.BtnRegister = new System.Windows.Forms.Button();
+            this.BtnFind = new System.Windows.Forms.Button();
+            this.BtnShow = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Header
+            // 
+            this.Header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(135)))), ((int)(((byte)(31)))));
+            this.Header.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Header.Font = new System.Drawing.Font("Mokhtar Font", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Header.ForeColor = System.Drawing.Color.White;
+            this.Header.Location = new System.Drawing.Point(-1, 0);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(885, 60);
+            this.Header.TabIndex = 0;
+            this.Header.Text = "Wecome To USP Prod.";
+            this.Header.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BackGround
+            // 
+            this.BackGround.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(185)))), ((int)(((byte)(20)))));
+            this.BackGround.Location = new System.Drawing.Point(-4, 60);
+            this.BackGround.Name = "BackGround";
+            this.BackGround.Size = new System.Drawing.Size(888, 506);
+            this.BackGround.TabIndex = 1;
+            // 
+            // TxtId
+            // 
+            this.TxtId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtId.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TxtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtId.Location = new System.Drawing.Point(293, 166);
+            this.TxtId.MaxLength = 50;
+            this.TxtId.Name = "TxtId";
+            this.TxtId.Size = new System.Drawing.Size(373, 26);
+            this.TxtId.TabIndex = 1;
+            // 
+            // toolTip
+            // 
+            this.toolTip.IsBalloon = true;
+            this.toolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // TxtName
+            // 
+            this.TxtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TxtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtName.Location = new System.Drawing.Point(293, 231);
+            this.TxtName.MaxLength = 50;
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(373, 26);
+            this.TxtName.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TxtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPassword.Location = new System.Drawing.Point(293, 294);
+            this.TxtPassword.MaxLength = 50;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(373, 26);
+            this.TxtPassword.TabIndex = 3;
+            // 
+            // LblId
+            // 
+            this.LblId.AutoSize = true;
+            this.LblId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(185)))), ((int)(((byte)(20)))));
+            this.LblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblId.ForeColor = System.Drawing.Color.White;
+            this.LblId.Location = new System.Drawing.Point(193, 166);
+            this.LblId.Name = "LblId";
+            this.LblId.Size = new System.Drawing.Size(99, 26);
+            this.LblId.TabIndex = 4;
+            this.LblId.Text = "ID       : ";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(185)))), ((int)(((byte)(20)))));
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(173, 231);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(117, 26);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name    : ";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(185)))), ((int)(((byte)(20)))));
+            this.LblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPassword.ForeColor = System.Drawing.Color.White;
+            this.LblPassword.Location = new System.Drawing.Point(153, 294);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(137, 26);
+            this.LblPassword.TabIndex = 6;
+            this.LblPassword.Text = "Password : ";
+            // 
+            // BtnRegister
+            // 
+            this.BtnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(135)))), ((int)(((byte)(31)))));
+            this.BtnRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnRegister.FlatAppearance.BorderSize = 0;
+            this.BtnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegister.ForeColor = System.Drawing.Color.White;
+            this.BtnRegister.Location = new System.Drawing.Point(158, 399);
+            this.BtnRegister.Name = "BtnRegister";
+            this.BtnRegister.Size = new System.Drawing.Size(103, 38);
+            this.BtnRegister.TabIndex = 7;
+            this.BtnRegister.Text = "Register";
+            this.BtnRegister.UseVisualStyleBackColor = false;
+            this.BtnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
+            // 
+            // BtnFind
+            // 
+            this.BtnFind.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(135)))), ((int)(((byte)(31)))));
+            this.BtnFind.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnFind.FlatAppearance.BorderSize = 0;
+            this.BtnFind.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFind.ForeColor = System.Drawing.Color.White;
+            this.BtnFind.Location = new System.Drawing.Point(293, 399);
+            this.BtnFind.Name = "BtnFind";
+            this.BtnFind.Size = new System.Drawing.Size(103, 38);
+            this.BtnFind.TabIndex = 8;
+            this.BtnFind.Text = "Find";
+            this.BtnFind.UseVisualStyleBackColor = false;
+            this.BtnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // BtnShow
+            // 
+            this.BtnShow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(135)))), ((int)(((byte)(31)))));
+            this.BtnShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnShow.FlatAppearance.BorderSize = 0;
+            this.BtnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnShow.ForeColor = System.Drawing.Color.White;
+            this.BtnShow.Location = new System.Drawing.Point(430, 399);
+            this.BtnShow.Name = "BtnShow";
+            this.BtnShow.Size = new System.Drawing.Size(103, 38);
+            this.BtnShow.TabIndex = 9;
+            this.BtnShow.Text = "Show ";
+            this.BtnShow.UseVisualStyleBackColor = false;
+            this.BtnShow.Click += new System.EventHandler(this.BtnShow_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(78)))), ((int)(((byte)(20)))));
+            this.BtnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnExit.FlatAppearance.BorderSize = 0;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.ForeColor = System.Drawing.Color.White;
+            this.BtnExit.Location = new System.Drawing.Point(563, 399);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(103, 38);
+            this.BtnExit.TabIndex = 10;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnShow);
+            this.Controls.Add(this.BtnFind);
+            this.Controls.Add(this.BtnRegister);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.LblId);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.BackGround);
+            this.Controls.Add(this.Header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 39);
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Header;
+        private System.Windows.Forms.Label BackGround;
+        private System.Windows.Forms.TextBox TxtId;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Label LblId;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.Button BtnRegister;
+        private System.Windows.Forms.Button BtnFind;
+        private System.Windows.Forms.Button BtnShow;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
+
---
+++ EnterData/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EnterData
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent(); 
+            
+
+        }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader file_ = new StreamReader("Data.txt");
+                string ReadFileData = file_.ReadLine();
+                file_.Close(); 
+
+                StreamWriter file = new StreamWriter("Data.txt", true);
+                string data = TxtId.Text + ";" + TxtName.Text + ";" + TxtPassword.Text;
+                //file.Close();
+                if (TxtId.Text.Length == 0 || TxtName.Text.Length == 0 || TxtPassword.Text.Length == 0)
+                {
+
+                    string ErrorMessage = "Please enter all your Data"; 
+                    MessageBox.Show(ErrorMessage);
+                    TxtId.Focus();
+                    return; 
+                    
+                }
+                if ( TxtPassword.TextLength < 4)
+                {
+
+                    MessageBoxIcon icon = MessageBoxIcon.Warning; 
+                    MessageBox.Show("Your password is weak.", "Password weak" , 0, icon);
+                    TxtPassword.Focus();
+                    TxtPassword.SelectAll();
+                    return; 
+
+                }
+
+                if (ReadFileData.Contains(TxtId.Text + ";"))
+                {
+                    MessageBox.Show("This user already exist!");
+                    return; 
+                }
+
+                file.WriteLine(data);
+                TxtId.Clear();
+                TxtName.Clear();
+                TxtPassword.Clear();
+                MessageBox.Show("Done");
+                file.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            string msj = "Do you want to exit?";
+            MessageBoxButtons btn = MessageBoxButtons.YesNo;
+            MessageBoxIcon icon = MessageBoxIcon.Warning;
+            DialogResult result = MessageBox.Show(msj, "Allert", btn, icon); 
+            if ( result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+             
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            Form ShowData = new Form();
+            TextBox txtShow = new TextBox(); 
+            ShowData.StartPosition = FormStartPosition.CenterScreen;
+            ShowData.Size = new System.Drawing.Size(300, 300);
+            txtShow.Multiline = true;
+            txtShow.Font = new Font(this.Font.ToString(), 9, FontStyle.Bold); 
+            txtShow.ReadOnly = true; 
+            txtShow.Dock = DockStyle.Fill; 
+
+            try 
+            {
+                StreamReader file = new StreamReader("Data.txt");
+                string GetData = file.ReadToEnd();
+                file.Close();
+                txtShow.Text = GetData;
+                ShowData.Controls.Add(txtShow); 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message); 
+            }
+            ShowData.ShowDialog(); 
+
+
+        }
+
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            bool found = false; 
+            if (TxtId.Text.Trim() != "")
+            {
+                StreamReader file = new StreamReader("Data.txt");
+                string GetData = "";
+                do
+                {
+
+                    GetData = file.ReadLine();
+                    if ( GetData != null)
+                    { 
+                        string[] Data = GetData.Split(';');
+                        if (Data[0] == TxtId.Text)
+                        {
+                            TxtName.Text = Data[1];
+                            TxtPassword.Text = Data[2];
+                            found = true;
+                       
+                            break;
+                        }
+                    }
+                    //else
+                    //{
+                    //    MessageBox.Show("User doesn't exist. ");
+                    //    file.Close();
+                    //    break;
+                    //}
+
+
+                } while (GetData != null);
+
+            file.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter an Id"); 
+            }
+
+            if (!found)
+            {
+                MessageBox.Show("user not found"); 
+            }
+        }
+    }
+}
